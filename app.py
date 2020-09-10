eee4eacc584327f94520ab9724ae8e457669ae00
Lauren505 <59271626+Lauren505@users.noreply.github.com>
2020-09-10 16:22:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ line_bot_api = LineBotApi('tG2+BvqixjrHDIX6jhmCcW+g5LMa4fumy+Zm6PdnyFJleHpK0F1pn
 # Channel Secret
 handler = WebhookHandler('4028d87a97d982c2049e4a0ad2131698')
 
-conn = psycopg2.connect(database=db395cakuikd2s,user=yoxejfdazwyghe,password=b35aaaceaf6c2c2bb47b3054e364fefec42fb8be687364e35e6b58a2260da715,host=ec2-52-202-198-60.compute-1.amazonaws.com,port=5432)
+conn = psycopg2.connect(database='db395cakuikd2s',user='yoxejfdazwyghe',password='b35aaaceaf6c2c2bb47b3054e364fefec42fb8be687364e35e6b58a2260da715',host='ec2-52-202-198-60.compute-1.amazonaws.com',port='5432')
 cur = conn.cursor()
 
 cur.execute('SELECT VERSION()')
