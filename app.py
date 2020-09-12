bce5ebdfba13ec4c8b123d7b026e2ba2bd593a73
Lauren505 <59271626+Lauren505@users.noreply.github.com>
2020-09-12 13:18:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -81,13 +81,14 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
     else:
         message = TextSendMessage(text=msg)
-        # cur.execute("INSERT INTO profile(school, studentid) VALUES (%s, %s)", ("大葉", "B00"))
+        cur.execute("INSERT INTO profile(school, studentid) VALUES (%s, %s)", ("大葉", "B00"))
         cur.execute("SELECT * FROM profile")
         rows = cur.fetchall()
         print('fetch from profile...')
         for row in rows:
             print(row)
         line_bot_api.reply_message(event.reply_token, message)
+        conn.commit()
 
 import os
 if __name__ == "__main__":
