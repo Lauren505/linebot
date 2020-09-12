4fa447e0a35687cdd86de9d84c386cd54f28d630
Lauren505 <59271626+Lauren505@users.noreply.github.com>
2020-09-12 12:25:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event)
     msg = event.message.text
     if '最新合作廠商' in msg:
         message = imagemap_message()
