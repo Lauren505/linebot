ec5e132fbb6f32941a733020b6daff9a26b67ba0
Lauren505 <59271626+Lauren505@users.noreply.github.com>
2020-09-20 14:23:24 +0800

    Update postgresql.py

--- postgresql.py
+++ postgresql.py
@@ -55,6 +55,7 @@ class postgre:
                         (userid, status, university, department, studentid, year_of_enrollment, name, student_id_card, lineid))
         self.conn.commit()
     
+    # Set user status
     def setUserStatus(self, userid, status):
         status = str(status)
         self.cur.execute("""
@@ -64,8 +65,53 @@ class postgre:
                         """, (status, userid))
         self.conn.commit()
 
-    def setQuestions(self):
-        pass
+    # Set questions
+    def setQuestion(self, question_id, asker_id, question_image, question_text, status, subject, source, number):
+        self.cur.execute("INSERT INTO questions(question_id, asker_id, question_image, question_text, status, subject, source, number) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                        (question_id, asker_id, question_image, question_text, status, subject, source, number))
+        self.conn.commit()
 
-    def getQuestions(self):
-        return 0
+    # Functions for data retrieval...
+    def getQuestionList(self):
+        question_list = []
+        self.cur.execute("SELECT * FROM questions")
+        rows = self.cur.fetchall()
+        for row in rows:
+            header = [row[5], row[6], row[7]]
+            body = [row[3]]
+            question_list.append([row[0], header, row[2], body])
+        print(question_list)
+        return question_list
+
+    def getQuestionListBySubject(self, subject):
+        question_list = []
+        self.cur.execute("SELECT * FROM questions WHERE subject=%s", subject)
+        rows = self.cur.fetchall()
+        for row in rows:
+            header = [row[5], row[6], row[7]]
+            body = [row[3]]
+            question_list.append([row[0], header, row[2], body])
+        print(question_list)
+        return question_list
+
+    def getQuestionListByAsker(self, askerid):
+        question_list = []
+        self.cur.execute("SELECT * FROM questions WHERE asker_id=%s", askerid)
+        rows = self.cur.fetchall()
+        for row in rows:
+            header = [row[5], row[6], row[7]]
+            body = [row[3]]
+            question_list.append([row[0], header, row[2], body])
+        print(question_list)
+        return question_list
+
+    '''
+    question_list = [
+            # |題號|            header                |           圖片url                 |                  body中的文字
+            [1005, ['交換電路與邏輯設計', '課本', '7.1'], 'https://i.imgur.com/8Mjhu0Y.png',
+                ['第一段我有點不懂', '第二段我也不太懂', '第三段我也不會，我是不是太笨了QQ？']],
+            [1006, ['計算機概論', '作業',  '1'],
+                'https://i.imgur.com/FwVstGx.png', ['這次作業好難喔', '我都不會寫']],
+            [1007, ['微積分 ', '考古題', '108.1'],
+                'https://i.imgur.com/x3Sftiv.png', ['吉鈞救救我']]
+        ]'''
